000bb28ec8725cdc20cb6fa5143e3bcda1b5f334
wert2all <wert2all@gmail.com>
2024-06-08 13:17:04 +0300

    feat: create simple application routes

--- server.go
+++ server.go
@@ -4,10 +4,22 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.RealIP)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("hi"))
+		if err != nil {
+			log.Fatal("Could not write to response", err)
+		}
+	})
+
 	log.Fatal(http.ListenAndServe(":8000", router))
 }

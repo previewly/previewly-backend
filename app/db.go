19c68d1986085054d044c31da6e736b03e8ea35c
wert2all <wert2all@gmail.com>
2024-06-13 14:30:10 +0300

    refactor: refactor di

---
+++ app/db.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"context"
+	"strings"
+	"wsw/backend/ent"
+)
+
+func newDBClient(config Postgres, appContext context.Context) (*ent.Client, error) {
+	client, err := ent.Open("postgres", createConnectionURI(config))
+	if err != nil {
+		return nil, err
+	}
+	if err := client.Schema.Create(appContext); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func createConnectionURI(config Postgres) string {
+	var sb strings.Builder
+
+	optionsMap := map[string]string{
+		"host":     config.Host,
+		"port":     config.Port,
+		"user":     config.User,
+		"password": config.Password,
+		"dbname":   config.DB,
+		"sslmode":  "disable",
+	}
+
+	for key, val := range optionsMap {
+		sb.WriteString(key + "=" + val + " ")
+	}
+
+	return sb.String()
+}

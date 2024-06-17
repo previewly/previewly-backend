05d1f044de7fe6c28bee89f51d188b47e08b1d4f
wert2all <wert2all@gmail.com>
2024-06-17 14:56:59 +0300

    feat: adding url on model

--- model/url/model.go
+++ model/url/model.go
@@ -2,6 +2,7 @@ package url
 
 import (
 	"wsw/backend/domain/preview"
+	"wsw/backend/ent"
 	"wsw/backend/ent/repository"
 )
 
@@ -14,9 +15,33 @@ type (
 	}
 )
 
+func (u urlImpl) getUrlEntity(url string) (*ent.Url, error) {
+	urlEntity := u.repository.TryGet(url)
+	if urlEntity == nil {
+		urlEntity, err := u.repository.Insert(url)
+		if err != nil {
+			return nil, err
+		}
+		return urlEntity, nil
+	}
+	return urlEntity, nil
+}
+
+func (u urlImpl) updateUrlData(erl *ent.Url) (*preview.PreviewData, error) {
+	panic("not implemented")
+}
+
 // AddURL implements Token.
 func (u urlImpl) AddURL(url string) (*preview.PreviewData, error) {
-	panic("unimplemented")
+	urlEntity, err := u.getUrlEntity(url)
+	if err != nil {
+		return nil, err
+	}
+	preview, errPreview := u.updateUrlData(urlEntity)
+	if errPreview != nil {
+		return nil, errPreview
+	}
+	return preview, nil
 }
 
 func NewUrl(urlRepository repository.Url) Url {

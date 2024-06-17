7a6b0b0c10d8eb86b222c11a0deb012f6f954e4c
wert2all <wert2all@gmail.com>
2024-06-17 14:56:59 +0300

    feat: adding url on model

--- ent/repository/url.go
+++ ent/repository/url.go
@@ -6,12 +6,26 @@ import (
 )
 
 type (
-	Url     interface{}
+	Url interface {
+		TryGet(string) *ent.Url
+		Insert(string) (*ent.Url, error)
+	}
+
 	urlImpl struct {
 		client *ent.Client
 		ctx    context.Context
 	}
 )
+
+// Insert implements Url.
+func (u *urlImpl) Insert(string) (*ent.Url, error) {
+	panic("unimplemented")
+}
+
+// TryGet implements Url.
+func (u *urlImpl) TryGet(string) *ent.Url {
+	panic("unimplemented")
+}
 
 func NewUrl(client *ent.Client, ctx context.Context) Url {
 	return &urlImpl{client: client, ctx: ctx}

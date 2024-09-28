45896dd4d523cadbf15f6a180b5a33b1349bcd0c
Serhii Shvets <wert2all@gmail.com>
2024-09-28 18:12:58 +0300

    fix: remove useless methods

--- ent/repository/url.go
+++ ent/repository/url.go
@@ -12,7 +12,6 @@ type (
 	Url interface {
 		TryGet(string) *ent.Url
 		Insert(string) (*ent.Url, error)
-		UpdateApiUrlId(*ent.Url, int) error
 		Update(string, url.Status, int) (*ent.Url, error)
 	}
 
@@ -28,18 +27,12 @@ func (u *urlImpl) Update(image string, status url.Status, ID int) (*ent.Url, err
 	if err != nil {
 		return nil, err
 	}
-	return u.client.Url.UpdateOne(urlEntity).SetImage(image).SetStatus(status).Save(u.ctx)
-}
-
-// UpdateApiUrlId implements Url.
-func (u *urlImpl) UpdateApiUrlId(url *ent.Url, apiUrlId int) error {
-	_, err := u.client.Url.UpdateOne(url).SetAPIURLID(apiUrlId).Save(u.ctx)
-	return err
+	return u.client.Url.UpdateOne(urlEntity).SetImageURL(image).SetStatus(status).Save(u.ctx)
 }
 
 // Insert implements Url.
 func (u *urlImpl) Insert(url string) (*ent.Url, error) {
-	return u.client.Url.Create().SetURL(url).SetStatus("pending").SetImage("").Save(u.ctx)
+	return u.client.Url.Create().SetURL(url).SetStatus("pending").SetImageURL("").Save(u.ctx)
 }
 
 // TryGet implements Url.

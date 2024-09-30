0a77f5b8e0a84169181ab34d4c3d295448e88a63
wert2all <wert2all@gmail.com>
2024-09-30 11:30:53 +0300

    fix: fix non-random tokens

--- domain/token/generator/token.go
+++ domain/token/generator/token.go
@@ -2,38 +2,24 @@ package generator
 
 import (
 	"fmt"
-	"wsw/backend/lib/utils"
+	"strings"
 
-	"golang.org/x/exp/rand"
+	"github.com/google/uuid"
 )
 
 type (
 	TokenGenerator interface {
 		Generate() string
 	}
-	tokenGeneratorImpl struct {
-		stringLeng int
-		letters    []rune
-	}
+	tokenGeneratorImpl struct{}
 )
 
 // Generate implements TokenGenerator.
 func (t *tokenGeneratorImpl) Generate() string {
-	return fmt.Sprintf("%s-%s-%s", t.randomString(), t.randomString(), t.randomString())
-}
-
-func (t *tokenGeneratorImpl) randomString() string {
-	b := make([]rune, t.stringLeng)
-	for i := range b {
-		b[i] = t.letters[rand.Intn(len(t.letters))]
-	}
-	return string(b)
+	splitted := strings.Split(uuid.New().String(), "-")
+	return fmt.Sprintf("%s-%s-%s", splitted[1], splitted[2], splitted[3])
 }
 
 func NewTokenGenerator() TokenGenerator {
-	utils.InitRandom()
-	return &tokenGeneratorImpl{
-		letters:    []rune("abcdefghijklmnopqrstuvwxyz1234567890"),
-		stringLeng: 4,
-	}
+	return &tokenGeneratorImpl{}
 }

a67ae61e17c74676d3c084b11f21350e91426d80
Serhii Shvets <wert2all@gmail.com>
2024-12-19 18:10:29 +0200

    refactor: rename image file name generator

--- domain/image/upload/storage/filenameGenerator.go
+++ domain/image/upload/storage/filenameGenerator.go
@@ -8,15 +8,15 @@ import (
 )
 
 type (
-	FilenameProvider interface {
+	FilenameGenerator interface {
 		GenerateFilename(string) string
 		GenerateFilepath(*string) string
 	}
-	filenameProviderImpl struct{}
+	filenameGeneratorImpl struct{}
 )
 
 // GenerateFilepath implements FilenameProvider.
-func (f filenameProviderImpl) GenerateFilepath(prefix *string) string {
+func (f filenameGeneratorImpl) GenerateFilepath(prefix *string) string {
 	path := fmt.Sprintf("%d/%d/%d", time.Now().Year(), time.Now().Month(), time.Now().Day())
 	if prefix != nil {
 		return strings.Join([]string{path, *prefix}, "/")
@@ -26,10 +26,10 @@ func (f filenameProviderImpl) GenerateFilepath(prefix *string) string {
 }
 
 // GenerateFilename implements FilenameProvider.
-func (f filenameProviderImpl) GenerateFilename(filename string) string {
+func (f filenameGeneratorImpl) GenerateFilename(filename string) string {
 	return fmt.Sprintf("%d%s", time.Now().UnixNano(), filepath.Ext(filename))
 }
 
-func NewFilenameProvider() FilenameProvider {
-	return filenameProviderImpl{}
+func NewFilenameProvider() FilenameGenerator {
+	return filenameGeneratorImpl{}
 }

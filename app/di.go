13112502aba312210d695948672c343d1959c0b8
wert2all <wert2all@gmail.com>
2024-12-22 16:30:39 +0200

    feat: generate image url

--- app/di.go
+++ app/di.go
@@ -124,8 +124,8 @@ func initDomains(config config.Config) {
 		return domainStorage.NewUploadStorage(filenameGenerator, pathProvider)
 	})
 	initService(func() process.Convertor { return process.NewConvertor() })
-	initService(func(pathProvider domainImagePath.PathProvider, pathGenerator domainImagePath.FilenameGenerator) domainRunner.ProcessRunner {
-		return domainRunner.NewProcessRunner(pathProvider, pathGenerator)
+	initService(func(pathProvider domainImagePath.PathProvider, pathGenerator domainImagePath.FilenameGenerator, urlProvider domainImageUrl.Provider) domainRunner.ProcessRunner {
+		return domainRunner.NewProcessRunner(pathProvider, pathGenerator, urlProvider)
 	})
 }
 

a14094746d1322088c7dca77629b5635c89d2525
wert2all <wert2all@gmail.com>
2025-01-31 13:26:43 +0200

    fix: fix gql server

--- app/router.go
+++ app/router.go
@@ -1,16 +1,25 @@
 package app
 
 import (
+	"context"
 	"net/http"
 
 	"wsw/backend/graph"
 
-	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/go-chi/chi/v5"
+	"github.com/vektah/gqlparser/v2/ast"
+	"github.com/vektah/gqlparser/v2/gqlerror"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 )
 
 func newRouter(midlewares Middlewares) *chi.Mux {
-	gqlHandler := createGQLHandler()
+	srv := createGQLServer(createSchema())
+	gqlHandler := func(w http.ResponseWriter, r *http.Request) { srv.ServeHTTP(w, r) }
 
 	router := chi.NewRouter()
 	router.Use(midlewares.List...)
@@ -25,8 +34,27 @@ func newRouter(midlewares Middlewares) *chi.Mux {
 	return router
 }
 
-func createGQLHandler() http.HandlerFunc {
-	scheme := graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}})
-	server := handler.NewDefaultServer(scheme)
-	return func(w http.ResponseWriter, r *http.Request) { server.ServeHTTP(w, r) }
+func createSchema() graphql.ExecutableSchema {
+	return graph.NewExecutableSchema(
+		graph.Config{Resolvers: &graph.Resolver{}},
+	)
+}
+
+func createGQLServer(schema graphql.ExecutableSchema) *handler.Server {
+	srv := handler.New(schema)
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.MultipartForm{})
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	srv.SetRecoverFunc(func(ctx context.Context, err interface{}) error {
+		return gqlerror.Errorf("Internal server error!")
+	})
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+	return srv
 }
